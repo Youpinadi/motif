50b248bde03e6b6fc31770c684cf0364d31337db
Nadir Kadem <nadir.kadem@gmail.com>
2018-05-21 23:43:45 +0200

    multiple cogs

--- src/App.js
+++ src/App.js
@@ -8,19 +8,15 @@ import './App.css';
 const CANVAS_SIZE = window.innerHeight;
 const SPRING_PARAM = { stiffness: 60, damping: 9 };
 
-const CentralShape = ({ appDimensions, size, color, children }) => {
-  const { width, height } = appDimensions;
-  const centerX = (width - size) / 2;
-  const centerY = (height - size) / 2;
-
+const CentralShape = ({ x, y, size, color, children }) => {
   return (
     <Motion
       defaultStyle={{ top: 0, left: 0, width: 0, height: 0 }}
       style={{
         width: spring(size, SPRING_PARAM),
         height: spring(size, SPRING_PARAM),
-        top: spring(centerY),
-        left: spring(centerX)
+        top: spring(y),
+        left: spring(x)
       }}
     >
       {interpolatingStyle => (
@@ -86,6 +82,29 @@ const Hiders = ({
     ));
 };
 
+const Cog = ({ x, y, size, color }) => (
+  <CentralShape x={x} y={y} size={size} color={color}>
+    <Hiders
+      shapeSize={size}
+      offset={size / 2}
+      offsetRotation={random(100)}
+      nbHiders={random(10, 15)}
+      hiderBorderRadius={random(size / 3) + 10}
+      hiderBorderRadius2={random(size / 3) + 10}
+      hiderSize={random(size / 3) + 10}
+    />
+    <Hiders
+      shapeSize={size}
+      offset={size / 4}
+      offsetRotation={random(100)}
+      nbHiders={random(5, 10)}
+      hiderBorderRadius={random(50)}
+      hiderBorderRadius2={random(50)}
+      hiderSize={random(size / 10) + 5}
+    />
+  </CentralShape>
+);
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -106,6 +125,36 @@ class App extends Component {
     });
   };
 
+  renderCogs(width, height) {
+    const cols = 3;
+    const rows = 2;
+    const tileWidth = width / cols;
+    const tileHeight = height / rows;
+
+    const shapeSize = Math.max(tileWidth, tileHeight) - 200;
+    const { shapeColor } = this.state;
+
+    let cogs = [];
+    for (let i = 0; i < cols; i++) {
+      for (let j = 0; j < rows; j++) {
+        const x = i * tileWidth;
+        const y = j * tileHeight;
+
+        cogs.push(
+          <Cog
+            x={x}
+            y={y}
+            size={shapeSize}
+            color={randomColor({
+              luminosity: 'bright'
+            })}
+          />
+        );
+      }
+    }
+    return cogs;
+  }
+
   renderApp = ({ width, height }) => {
     const { shapeColor } = this.state;
     const appSize = Math.min(width, height);
@@ -119,32 +168,8 @@ class App extends Component {
             width,
             height
           }}
-        >
-          <CentralShape
-            appDimensions={{ width, height }}
-            size={shapeSize}
-            color={shapeColor}
-          >
-            <Hiders
-              shapeSize={shapeSize}
-              offset={shapeSize / 2}
-              offsetRotation={random(100)}
-              nbHiders={random(10, 15)}
-              hiderBorderRadius={random(shapeSize / 3) + 10}
-              hiderBorderRadius2={random(shapeSize / 3) + 10}
-              hiderSize={random(shapeSize / 3) + 10}
-            />
-            <Hiders
-              shapeSize={shapeSize}
-              offset={shapeSize / 4}
-              offsetRotation={random(100)}
-              nbHiders={random(5, 10)}
-              hiderBorderRadius={random(50)}
-              hiderBorderRadius2={random(50)}
-              hiderSize={random(shapeSize / 10) + 5}
-            />
-          </CentralShape>
-        </div>
+        />
+        {this.renderCogs(width, height)}
       </div>
     );
   };
